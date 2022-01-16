9ac9918b6b4cb2318355fee05bb16b0c6deac37d
Jacob Matson <matsonj@gmail.com>
2022-01-16 10:06:20 -0800

    linking to dbt

--- models/letters/most_used_letter.sql
+++ models/letters/most_used_letter.sql
@@ -2,31 +2,31 @@ with letters as (
 
 SELECT
 	SUBSTRING(word, 1, 1) letter
-from public.wordle
+from {{ ref( 'wordle' ) }}
 
 union all 
 
 SELECT
 	SUBSTRING(word, 2, 1) letter
-from public.wordle
+from {{ ref( 'wordle' ) }}
 
 union all 
 
 SELECT
 	SUBSTRING(word, 3, 1) letter
-from public.wordle
+from {{ ref( 'wordle' ) }}
 
 union all 
 
 SELECT
 	SUBSTRING(word, 4, 1) letter
-from public.wordle
+from {{ ref( 'wordle' ) }}
 
 union all 
 
 SELECT
 	SUBSTRING(word, 5, 1) letter
-from public.wordle
+from {{ ref( 'wordle' ) }}
 
 )
 
