53e3ef4521316a156e055ea30ce254f6b3fc55fe
Jacob Matson <matsonj@gmail.com>
2022-01-16 13:49:56 -0800

    notes

--- models/first_word_fast/final_wordlist.sql
+++ models/first_word_fast/final_wordlist.sql
@@ -5,3 +5,5 @@ join {{ ref( 'guesses' ) }} guess
 order by summary.avg desc
 limit 500
 -- limit is based on visual inspection of matches which shows a breakpoint around the top 500 letters
+-- the assumption is that it is unlikely that both words in the ideal two word combination will be outside the
+-- 500 word limit
